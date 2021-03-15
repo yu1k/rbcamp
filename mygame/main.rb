5f9beb7786014dfdfcce3c3129eb4f51d4790153
yu1k <48705423+yu1k@users.noreply.github.com>
2021-03-15 11:56:22 +0900

    Fix typo

--- mygame/main.rb
+++ mygame/main.rb
@@ -3,11 +3,13 @@ require "dxruby"
 Window.width = 800
 Window.height = 600
 
-image = Image.load("./image/player.png")
+image = Image.load("./images/player.png")
 
 Window.loop do
     # 1秒間に60回繰り返す
 
     # エスケープキーを押すとループが止まる処理
     break if Input.key_push?(K_ESCAPE)
+
+    Window.draw(0, 0, image)
 end
