91bebe802d9f7c0ab240f760aad02a525b036379
yu1k <48705423+yu1k@users.noreply.github.com>
2021-03-15 11:52:27 +0900

    Fix typo

--- mygame/main.rb
+++ mygame/main.rb
@@ -6,7 +6,7 @@ Window.height = 600
 image = Image.load("./image/player.png")
 
 Window.loop do
-    # q秒間に60回繰り返す
+    # 1秒間に60回繰り返す
 
     # エスケープキーを押すとループが止まる処理
     break if Input.key_push?(K_ESCAPE)
